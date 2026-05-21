da37ca6f550a42d015377ab94ec8efb84ad965ac
Kevin <kazakov28082003@gmail.com>
2026-05-21 09:53:24 +0900

    BRR: Initinale SetUp

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+console.log("EXECUTED!");
+
+import moment from "moment";
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime);
+
+// const person: string = "Martin";
+// const age: number = 30;
+
+// let arr: string[];
+// arr = ["Olma", "behi", "hurmo"];
