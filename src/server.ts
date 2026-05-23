757f5e1652a05517a886c6df36ad2c0b0386a8d9
Kevin <kazakov28082003@gmail.com>
2026-05-24 00:06:24 +0900

    feat: integretion of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,4 @@
-console.log("EXECUTED!");
-
-import moment from "moment";
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-
-// const person: string = "Martin";
-// const age: number = 30;
-
-// let arr: string[];
-// arr = ["Olma", "behi", "hurmo"];
+import dotenv from "dotenv";
+dotenv.config();
+console.log("PORT", process.env.PORT);
+console.log("MOngo url", process.env.MONGO_URL);
