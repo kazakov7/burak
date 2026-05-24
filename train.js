bb723ac7b93d7995b3a03c1d8c153b8ba79b0560
Kevin <kazakov28082003@gmail.com>
2026-05-24 10:25:49 +0900

    feat: complated MIT TASK M

--- train.js
+++ train.js
@@ -1,9 +1,18 @@
-//MIT TASK L
-function revSent(str) {
-  arr = [];
-  for (word of str.split(" ")) {
-    arr.push(word.split("").reverse().join(""));
+//MIT TASK "M"
+function getSquNums(arr) {
+  arr2 = [];
+  for (num of arr) {
+    arr2.push({ number: num, square: num * num });
   }
-  return arr.join(" ");
+  return arr2;
 }
-console.log(revSent("we like coding"));
+console.log(getSquNums([1, 2, 3, 4]));
+//MIT TASK L
+// function revSent(str) {
+//   arr = [];
+//   for (word of str.split(" ")) {
+//     arr.push(word.split("").reverse().join(""));
+//   }
+//   return arr.join(" ");
+// }
+// console.log(revSent("we like coding"));
