91a24788a8ba0c457c62acb7639e3e94b8c04e64
Kevin <kazakov28082003@gmail.com>
2026-05-28 08:07:32 +0900

    feat: complated mit task N

--- train.js
+++ train.js
@@ -1,12 +1,22 @@
-//MIT TASK "M"
-function getSquNums(arr) {
-  arr2 = [];
-  for (num of arr) {
-    arr2.push({ number: num, square: num * num });
+// //MIT TASK "M"
+function palindromCheck(word) {
+  let new_word = word;
+  if (new_word === word.split("").reverse().join("")) {
+    return true;
+  } else {
+    return false;
   }
-  return arr2;
 }
-console.log(getSquNums([1, 2, 3, 4]));
+console.log(palindromCheck("kiyik"));
+console.log(palindromCheck("nos"));
+// function getSquNums(arr) {
+//   arr2 = [];
+//   for (num of arr) {
+//     arr2.push({ number: num, square: num * num });
+//   }
+//   return arr2;
+// }
+// console.log(getSquNums([1, 2, 3, 4]));
 //MIT TASK L
 // function revSent(str) {
 //   arr = [];
