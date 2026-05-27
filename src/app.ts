f81a8e6ee58e4f5941fccfe4c456a46fc39a5c36
Kevin <kazakov28082003@gmail.com>
2026-05-28 07:08:54 +0900

    feat: learn routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./routers";
 // 1-Enterance
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,5 +12,6 @@ app.use(express.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 // 4-routes
+app.use("/", router);
 
 export default app;
