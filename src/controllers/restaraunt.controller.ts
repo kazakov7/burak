18a0d1be4dea7661e564c33a7382e8549afb264e
Kevin <kazakov28082003@gmail.com>
2026-06-07 05:32:48 +0900

    feat: create checkAtuhSessions api

--- src/controllers/restaraunt.controller.ts
+++ src/controllers/restaraunt.controller.ts
@@ -3,6 +3,8 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/member.service";
 import { MemberType } from "../libs/enums/member.enum";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
+import { Message } from "../libs/errors";
+import session from "express-session";
 
 const memberService = new MemberService();
 
@@ -64,6 +66,20 @@ restarauntController.processSignup = async (
     req.session.save(function () {
       res.send(result);
     });
+  } catch (err) {
+    console.log("Error processSignup:", err);
+    res.send(err);
+  }
+};
+restarauntController.checkAuthSession = async (
+  req: AdminRequest,
+  res: Response,
+) => {
+  try {
+    console.log("processSignup");
+    if (req.session?.member)
+      res.send(`<script>alert("${req.session.member.memberNick}")</script>`);
+    else res.send(`<script>alert("${Message.NOT_AUTHENTICATED}")</script>`);
   } catch (err) {
     console.log("Error processSignup:", err);
     res.send(err);
--- src/libs/errors.ts
+++ src/libs/errors.ts
@@ -18,6 +18,7 @@ export enum Message {
   USED_NICK_PHONE = "You are inserting already used phone or nick!",
   NO_MEMBER_NICK = "No member with that member nick!",
   WRONG_PASSWORD = "Wrong password, pleasse try again1",
+  NOT_AUTHENTICATED = "You are not authenticated, Please login first",
 }
 
 class Errors extends Error {
