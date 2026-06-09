e63190715dcbdb4a61aee1dc30ba99a80d80acb0
Kevin <kazakov28082003@gmail.com>
2026-06-10 08:12:35 +0900

    feat: upload files into server via multer

---
+++ src/libs/utils/uploader.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import multer from "multer";
+import { SocketAddress } from "net";
+const { v4 } = require("uuid");
+
+function getTargetImageStroage(address: any) {
+  return multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, `./uploads/${address}`);
+    },
+    filename: function (req, file, cb) {
+      console.log(file);
+      const extension = path.parse(file.originalname).ext;
+      const random_name = v4() + extension;
+      cb(null, random_name);
+    },
+  });
+}
+
+const makeUploader = (address: string) => {
+  const storage = getTargetImageStroage(address);
+  return multer({ storage: storage });
+};
+
+export default makeUploader;
+// const product_storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads/products");
+//   },
+//   filename: function (req, file, cb) {
+//     console.log(file);
+//     const extension = path.parse(file.originalname).ext;
+//     const random_name = v4() + extension;
+//     cb(null, random_name);
+//   },
+// });
+
+// export const uploadProductImage = multer({ storage: product_storage });
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import restarauntController from "./controllers/restaraunt.controller";
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
+
 const router = express.Router();
 
 //Restoran
@@ -10,7 +12,11 @@ router
   .post("/login", restarauntController.processLogin);
 router
   .get("/signup", restarauntController.getSignup)
-  .post("/signup", restarauntController.processSignup);
+  .post(
+    "/signup",
+    makeUploader("members").single("memberImage"),
+    restarauntController.processSignup,
+  );
 router.get("/logout", restarauntController.logout);
 router.get("/check-me", restarauntController.checkAuthSession);
 
@@ -23,6 +29,7 @@ router.get(
 router.post(
   "/product/create",
   restarauntController.verifyRestaurant,
+  makeUploader("products").array("productImages", 5),
   productController.createNewProduct,
 );
 router.post(
@@ -30,6 +37,7 @@ router.post(
   restarauntController.verifyRestaurant,
   productController.updateChoosenProduct,
 );
+
 //User
 
 export default router;
