2bd5dd357156421738252398459bc187fda8c505
Kevin <kazakov28082003@gmail.com>
2026-06-18 07:08:31 +0900

    feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,17 @@
 console.log("Signup frontend javascript file");
+$(function () {});
+function validateSignUpForm() {
+  const memberNick = $(".member-nick").val();
+  const memberPhone = $(".member-phone").val();
+  const memberPassword = $(".member-password").val();
+  const confirmPassword = $(".confirm-password").val();
+
+  if (memberNick == "" || memberPhone == "" || memberPassword == "") {
+    alert("please, insert all required unputs!");
+    return false;
+  }
+  if (memberPassword != confirmPassword) {
+    alert("Password differse, pleaase check!");
+    return false;
+  }
+}
